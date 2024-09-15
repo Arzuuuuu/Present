e9e7a6569fc64f1311112d134ccfb0ef704ff2ad
Arzuuuuu <136372735+Arzuuuuu@users.noreply.github.com>
2024-09-15 16:48:15 +0100

    o

---
+++ script.js
@@ -0,0 +1,35 @@
+const messages = [
+    'Ops! Sembra che tu abbia bisogno di un po’ di pratica. Riprova!',
+    'Hmm, non è proprio giusto. Prova un’altra volta!',
+    'Quasi, ma non proprio. Forse prova un’altra combinazione?',
+    'Nope, non è quello giusto. Forse il tuo cane ha camminato sulla tastiera?',
+    'Oopsie! Password non trovata. Prova con la parola “amore”?'
+  ];
+  
+  let attemptCount = 0; // Contatore per il numero di tentativi errati
+  
+  function checkPassword() {
+    const correctPassword = 'amore';  // Imposta la password corretta
+    const passwordInput = document.getElementById('passwordInput').value;
+    const errorMessage = document.getElementById('errorMessage');
+    
+    if (passwordInput === correctPassword) {
+      // Ripristina il contatore dei tentativi
+      attemptCount = 0;
+      // Naviga alla pagina successiva se la password è corretta
+      window.location.href = './lettera/lettera.html';  // Sostituisci con la tua pagina successiva
+    } else {
+      // Mostra un messaggio divertente basato sul numero di tentativi
+      errorMessage.textContent = messages[attemptCount % messages.length];
+      errorMessage.style.display = 'block';
+      // Pulisci il campo di input della password
+      document.getElementById('passwordInput').value = '';
+      attemptCount++;
+    }
+  }
+  
+  // Opzionale: Nascondi il messaggio di errore quando l'utente inizia a digitare di nuovo
+  document.getElementById('passwordInput').addEventListener('input', function() {
+    document.getElementById('errorMessage').style.display = 'none';
+  });
+  
